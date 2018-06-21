c189aad1c1cd1b1ddfbfadf170190ed16d2db8e0
Mr. Rosenfeld <srosenfeld@users.noreply.github.com>
2018-06-21 09:45:31 -0400

    Add files via upload

--- first_pygame/intro.py
+++ first_pygame/intro.py
@@ -3,15 +3,33 @@
 import pgzrun
 
 alien = Actor('alien')
-alien.pos = 250, 400
+alien.topright = 0,10
 
-WIDTH = 500
-HEIGHT = 500
+WIDTH = alien.width + 500
+HEIGHT = alien.height + 200
 
 def draw():
     screen.clear()
     alien.draw()
 
+def update():
+    alien.right +=2
+    alien.y += 2
+    if alien.left > WIDTH:
+        alien.right = 0
+    if alien.y > HEIGHT:
+        alien.y = 10
 
+def on_mouse_down(pos):
+    if alien.collidepoint(pos):
+        set_alien_hurt()
+
+def set_alien_hurt():
+    alien.image = 'alien_hurt'
+    sounds.eep.play()
+    clock.schedule_unique(set_alien_normal, 1.0)
+
+def set_alien_normal():
+    alien.image = 'alien'
 
 pgzrun.go()
